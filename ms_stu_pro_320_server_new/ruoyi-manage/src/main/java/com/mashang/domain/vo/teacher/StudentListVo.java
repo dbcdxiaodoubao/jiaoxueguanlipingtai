bf69269a42686f9fa99c47174472ef34a3fd76a0
lizidernbpc <lizits@nb.com>
2025-11-21 18:00:40 +0800

    教师端学生列表vo时间格式优化

--- ms_stu_pro_320_server_new/ruoyi-manage/src/main/java/com/mashang/domain/vo/teacher/StudentListVo.java
+++ ms_stu_pro_320_server_new/ruoyi-manage/src/main/java/com/mashang/domain/vo/teacher/StudentListVo.java
@@ -1,5 +1,6 @@
 package com.mashang.domain.vo.teacher;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -31,6 +32,7 @@ public class StudentListVo {
     private String phonenumber;
 
     @ApiModelProperty("加入班级时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date joinClassTime;
 
     @ApiModelProperty("班级名称")
