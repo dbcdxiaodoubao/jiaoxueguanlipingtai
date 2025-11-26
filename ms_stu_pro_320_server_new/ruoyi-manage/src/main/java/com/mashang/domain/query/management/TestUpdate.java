67b0fe00b2999926396faa390bff018163e6d30d
xiaodoubao <16185824+xiaodoubaosgit@user.noreply.gitee.com>
2025-11-26 22:03:05 +0800

    1.优化了试卷修改的参数检验

--- ms_stu_pro_320_server_new/ruoyi-manage/src/main/java/com/mashang/domain/query/management/TestUpdate.java
+++ ms_stu_pro_320_server_new/ruoyi-manage/src/main/java/com/mashang/domain/query/management/TestUpdate.java
@@ -6,7 +6,9 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
@@ -40,5 +42,7 @@ public class TestUpdate {
     private Integer suggestDuration;
 
     @ApiModelProperty(value = "关联题目",required = true)
+    @NotEmpty(message = "题目不能为空")
+    @Valid
     private List<QuestionTestCreat> question;
 }
